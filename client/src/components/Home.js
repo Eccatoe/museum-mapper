a13a870230d244e2976ddd46d2bbf34c09045e90
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-03-27 22:02:54 -0400

    can't figure out geojson

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,10 @@ import MuseumContainer from "./MuseumContainer"
 
 function Home() {
   const [museums, setMuseums] = useState([])
+
+
+
+
   useEffect(() => {
     fetch("/museums")
       .then((r) => r.json())
