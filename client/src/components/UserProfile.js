7fe7b8f89b3edd7cdf763fa46e2faa95c3745a51
Nicole Candiotti <nccandiotti@gmail.com>
2022-03-31 22:30:37 -0400

    fixed scrolling and masonry

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,28 +1,30 @@
-import { useContext, useState, useEffect } from "react";
-import { UserContext } from "./UserContext";
-import UserTourCard from "./UserTourCard";
+import { useContext, useState, useEffect } from "react"
+import { UserContext } from "./UserContext"
+import UserTourCard from "./UserTourCard"
 
 function UserProfile() {
-  const { currentUser } = useContext(UserContext);
-  const [userTourArray, setUserTourArray] = useState([]);
+  const { currentUser } = useContext(UserContext)
+  const [userTourArray, setUserTourArray] = useState([])
 
   useEffect(() => {
     fetch("/user_tours")
       .then((r) => r.json())
-      .then((r) => setUserTourArray(r));
-  }, []);
+      .then((r) => setUserTourArray(r))
+  }, [])
 
-  console.log(userTourArray);
+  console.log(userTourArray)
   const createUserTourCards = userTourArray.map((userTour) => (
     <UserTourCard key={userTour.id} userTour={userTour} />
-  ));
+  ))
 
   return (
     <div id="portal">
       <h1>Welcome, {currentUser.first_name}</h1>
-      <div className="tourContainer">{createUserTourCards}</div>
+      <div className="tourContainer">
+        <div className="scroll">{createUserTourCards}</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default UserProfile;
+export default UserProfile
